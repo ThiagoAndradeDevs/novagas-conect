9dfdd72b8302af7527d93c26ab98a92910a3e0b5
Thiago <thiago.andrade.devs@gmail.com>
2021-12-22 14:48:31 -0300

    header

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,19 @@
+import react from 'react';
+
+function Header() {
+  return (
+    <header className='header'>
+      <div className='toolbar'>
+        <div>
+          <span>NovaGas Conect</span>
+        </div>
+        <div className=''>
+          <button>Novo Pedido</button>
+          <span>img1</span>
+          <span>img2</span>
+        </div>
+      </div>
+    </header>
+  )
+}
+export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import style from './style.css'
+import components from './components/Header';
+import Header from './components/Header';
 
 function Home() {
   return (
     <div>
-      <header>
-        header
-      </header>
-      <main>
-        main
+      <Header />
+      <main className='main'>
+        <div className='navbar'>navbar</div>
+        <div className='feed'>feed</div>
       </main>
     </div>
   )
