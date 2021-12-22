6eac1921127d08ab586fee10d9d5fc6af225b2d9
Thiago <thiago.andrade.devs@gmail.com>
2021-12-22 14:18:16 -0300

    home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <header>
+        header
+      </header>
+      <main>
+        main
+      </main>
+    </div>
+  )
+} export default Home;
