1fb876aaec34b78d9a70c2a28e7f7aff0bdc2724
Thiago <thiago.andrade.devs@gmail.com>
2021-12-27 15:20:09 -0300

    Typography

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
 
 
 
