f95bfa3c7c46824a021d1824c889aabb52a7a4ee
Thiago <thiago.andrade.devs@gmail.com>
2021-12-27 15:15:08 -0300

    Card

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
 
 
 
+const useStyles = makeStyles((theme) => {
+  {
+
+  }
+})
 
 
 function PostCard({ post }) {
+  const classes = useStyles();
 
   return (
     <h1>{post.description}</h1>
